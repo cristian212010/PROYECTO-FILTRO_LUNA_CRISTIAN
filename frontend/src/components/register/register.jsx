336719221419f6cfb302227eb2c3d8894e21fabe
Yisuusss:3 <mrlgsus22@gmail.com>
2023-10-09 14:32:09 -0500

    feat: :fire: Little change

--- frontend/src/components/register/register.jsx
+++ frontend/src/components/register/register.jsx
@@ -141,13 +141,3 @@ const Register = () => {
     </div>
   );
 };
-
-<<<<<<< HEAD
-export default Register;
-=======
-<<<<<<< HEAD
-export default Registro;
-=======
-export default Register;
->>>>>>> 8d7f337 (fix: :bug: Fix Register/login)
->>>>>>> Front-Authentication
