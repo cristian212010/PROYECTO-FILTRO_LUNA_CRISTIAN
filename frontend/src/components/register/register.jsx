fcefd02afb0a663dd1acbd88a9c8185ec252bb64
cristian212010 <cristiancamilo212010@gmail.com>
2023-10-10 07:37:18 -0500

    feat: :lipstick: Register Styles
    Se agregaron estilo al registro, reutilizando algunas clases del login

--- frontend/src/components/register/register.jsx
+++ frontend/src/components/register/register.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
+import logo from '../../assets/img/KARIO_LOGO.png';
+import '../../assets/styles/register.css'
 
 const Register = () => {
   const history = useHistory();
@@ -66,78 +68,80 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <h2>Registro de Usuario</h2>
-      {mensajeError && <p>{mensajeError}</p>}
-      <form onSubmit={submitRegister}>
-        <div>
-          <label>Nombre:</label>
-          <input
-            type="text"
-            name="nombre"
-            value={formData.nombre}
-            onChange={inputChange}
-            required
-          />
+    <div className='background'>
+      <div className='filter-login'>
+        <div className='form-register'>
+          <img src={logo} alt="KARIO" className='logo-login' />
+          <h2 className='h2-login'>Registro de Usuario</h2>
+          <p className='p-login'>Por favor ingresa los siguientes datos para ingresar a la plataforma</p>
+          {mensajeError && <p>{mensajeError}</p>}
+          <form onSubmit={submitRegister}>
+            <div className='div-input'>
+              <label>Nombre:</label>
+              <input
+                type="text"
+                name="nombre"
+                value={formData.nombre}
+                onChange={inputChange}
+                required
+                className='input-login'
+              />
+            </div>
+            <div className='div-input'>
+              <label>Apellido:</label>
+              <input
+                type="text"
+                name="apellido"
+                value={formData.apellido}
+                onChange={inputChange}
+                required
+                className='input-login'
+              />
+            </div>
+            <div className='div-input'>
+              <label>Usuario:</label>
+              <input
+                type="text"
+                name="usuario"
+                value={formData.usuario}
+                onChange={inputChange}
+                required
+                className='input-login'
+              />
+            </div>
+            <div className='div-input'>
+              <label>Password:</label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={inputChange}
+                required
+                className='input-login'
+              />
+            </div>
+            <div className='div-input'>
+              <label>Cargo:</label>
+              <select
+                name="cargo"
+                value={formData.cargo}
+                onChange={inputChange}
+                required
+                className='input-login'
+              >
+                <option value="">Seleccionar Cargo</option>
+                {cargos.map((cargo) => (
+                  <option key={cargo._id} value={cargo._id}>
+                    {cargo.cargo}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <button type="submit" className='btn-login'>Registrar</button>
+          </form>
+          <p className='p-registrar'>¿Ya tienes una cuenta? <Link to="/login">Iniciar sesión</Link></p>
         </div>
-        <div>
-          <label>Apellido:</label>
-          <input
-            type="text"
-            name="apellido"
-            value={formData.apellido}
-            onChange={inputChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Usuario:</label>
-          <input
-            type="text"
-            name="usuario"
-            value={formData.usuario}
-            onChange={inputChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={inputChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Cargo:</label>
-          <select
-            name="cargo"
-            value={formData.cargo}
-            onChange={inputChange}
-            required
-          >
-            <option value="">Seleccionar Cargo</option>
-            {cargos.map((cargo) => (
-              <option key={cargo._id} value={cargo._id}>
-                {cargo.cargo}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label>Avatar:</label>
-          <input
-            type="file"
-            name="avatar"
-            onChange={avatarChange}
-            accept="image/*"
-            required
-          />
-        </div>
-        <button type="submit">Registrar</button>
-      </form>
+      </div>
     </div>
   );
 };
