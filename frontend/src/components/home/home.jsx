6eb39184ba2cf0157f4935242217151d458c584c
cristian212010 <cristiancamilo212010@gmail.com>
2023-10-10 08:37:32 -0500

    feat: :lipstick: Navbar styles
    se creo el navbar pero faltan los iconos

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Navbar from '../navbar/navbar';
 
 const Home = () => {
 
@@ -10,6 +11,7 @@ const Home = () => {
 
   return (
     <div>
+      <Navbar></Navbar>
       <h2>  Inicio</h2>
       <p>Bienvenido al inicio.</p>
 
