009472b691ffa787d1334db091ecbf5a9700cfc0
Yisuusss:3 <mrlgsus22@gmail.com>
2023-10-10 13:53:56 -0500

    style: :lipstick: Description style
    with 3 points  instead line skip(...)

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -62,6 +62,53 @@ const Home = () => {
           }
         </table>
       </div>
+      <div className='content'>
+        <div className='text'>
+          <h1>Panel de Indicadores</h1>
+          <p>Aqui puedes visualizar los indicadores propuestos y añadidos por tu equipo de trabajo. Si quieres ver más detalles , dale click a uno de ellos para más información.</p>
+        </div>
+        <div>
+          <table class="tablita">
+            <thead>
+              <tr>
+                <th>Indicador</th>
+                <th>Descripción</th>
+                <th>Categoría</th>
+                <th>Fecha de Inicio</th>
+                <th>Fecha de Terminación</th>
+                <th>Fórmula</th>
+                <th>Frecuencia</th>
+                <th>Cumplimiento</th>
+                <th>Área</th>
+                <th></th>
+              </tr>
+            </thead>
+            {
+              APIData.map((data) => {
+                return (
+                  <tbody>
+                    <tr>
+                      <td>{data.indicador}</td>
+                      <td className="descripcion">{data.descripcion}</td>
+                      <td>{data.categoria}</td>
+                      <td>{data.fecha_inicio}</td>
+                      <td>{data.fecha_fin}</td>
+                      <td>{data.formula}</td>
+                      <td>{data.frecuencia}</td>
+                      <td> <CircularProgress value={data.cumplimiento} color={data.cumplimiento < 50 ? "red" : (data.cumplimiento >= 50 && data.cumplimiento <= 75) ? "orange" : "green"}><CircularProgressLabel><p className='porcentaje'><strong>{data.cumplimiento}%</strong></p></CircularProgressLabel> </CircularProgress></td>
+                      <td>{data.area[0].nombre}</td>
+                      <div className='icon'><GiIcons.GiHamburgerMenu></GiIcons.GiHamburgerMenu></div>
+                    </tr>
+                    <tr className="spacer">
+                      <td colspan="100"></td>
+                    </tr>
+                  </tbody>
+                )
+              })
+            }
+          </table>
+        </div>
+    </div>
         <button className='btn-add'>Añadir Elementos</button>
     </div>
   );
