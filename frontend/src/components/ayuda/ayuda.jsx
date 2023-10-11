2265abe8c03ae8138a6dd2170d6ab2d5de7ba738
S3bastianBernal <sebas201154@gmail.com>
2023-10-11 13:11:20 -0500

    feat: :sparkles: Renderizado Ayudas
    Se creo el Renderizado de la pagina de ayudas y se insertade manera dinamica

--- frontend/src/components/ayuda/ayuda.jsx
+++ frontend/src/components/ayuda/ayuda.jsx
@@ -7,6 +7,32 @@ import {
     Box
   } from '@chakra-ui/react'
 import Navbar from '../navbar/navbar'
+const preguntas_frecuentes = [
+    {
+      "pregunta": "¿Cómo puedo acceder a su sitio web?",
+      "respuesta": "Puedes acceder a nuestro sitio web escribiendo la URL en la barra de direcciones de tu navegador o haciendo clic en el enlace proporcionado en nuestros correos electrónicos o redes sociales."
+    },
+    {
+      "pregunta": "¿Qué información puedo encontrar en su sitio web?",
+      "respuesta": "En nuestro sitio web, puedes encontrar información sobre nuestros productos/servicios, precios, noticias, contacto y mucho más. También puedes acceder a recursos como guías y tutoriales."
+    },
+    {
+      "pregunta": "¿Cómo puedo ponerme en contacto con su equipo de soporte?",
+      "respuesta": "Para ponerse en contacto con nuestro equipo de soporte, puedes encontrar un enlace de 'Contacto' en la parte superior o inferior de nuestro sitio web. También proporcionamos números de teléfono y direcciones de correo electrónico para obtener asistencia."
+    },
+    {
+      "pregunta": "¿Cómo restablezco mi contraseña en su sitio web?",
+      "respuesta": "Para restablecer tu contraseña, ve a la página de inicio de sesión y busca el enlace '¿Olvidaste tu contraseña?'. Sigue las instrucciones para restablecerla a través de tu correo electrónico registrado."
+    },
+    {
+      "pregunta": "¿Cómo puedo actualizar mi información de perfil en el sitio web?",
+      "respuesta": "Inicia sesión en tu cuenta, ve a tu perfil y busca la opción 'Editar perfil' o una funcionalidad similar. Desde allí, podrás actualizar la información de tu perfil, como tu dirección de correo electrónico, contraseña, dirección, etc."
+    },
+    {
+      "pregunta": "¿Qué navegadores son compatibles con su sitio web?",
+      "respuesta": "Nuestro sitio web es compatible con la mayoría de los navegadores web populares, como Google Chrome, Mozilla Firefox, Microsoft Edge, Safari, y otros. Asegúrate de utilizar una versión actualizada de tu navegador para obtener la mejor experiencia."
+    }
+  ]
 
 
 const RenderAcordion = () => {
@@ -14,39 +40,27 @@ const RenderAcordion = () => {
         <div>
         <Navbar></Navbar>
         <Accordion allowToggle>
-            <AccordionItem>
-                <h2>
-            <AccordionButton>
-                <Box as="span" flex='1' textAlign='left'>
-                Section 1 title
-                </Box>
-                <AccordionIcon />
-            </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat.
-            </AccordionPanel>
-        </AccordionItem>
+            {
+                preguntas_frecuentes.map((data)=>{
+                    return(
+                    <AccordionItem>
+                        <h2>
+                        <AccordionButton>
+                            <Box as="span" flex='1' textAlign='left'>
+                            {data.pregunta}
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                        </h2>
+                        <AccordionPanel pb={4}>
+                        {data.respuesta}
+                        </AccordionPanel>
+                    </AccordionItem>
+                    )
+                })
+            }
 
-        <AccordionItem>
-            <h2>
-            <AccordionButton>
-                <Box as="span" flex='1' textAlign='left'>
-                Section 2 title
-                </Box>
-                <AccordionIcon />
-            </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat.
-            </AccordionPanel>
-        </AccordionItem>
+
         </Accordion>
         </div>
     )
