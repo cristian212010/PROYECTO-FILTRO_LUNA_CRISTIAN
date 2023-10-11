284aa4051639dc05486f213e6b1a9d1fba052fad
Yisuusss:3 <mrlgsus22@gmail.com>
2023-10-11 16:06:16 -0500

    fix: :rotating_light: fix import

--- Backend/src/server/server.js
+++ Backend/src/server/server.js
@@ -6,7 +6,6 @@ import loginRoutes from '../routes/login.routes.js';
 import reportesRoutes from '../routes/reportes.routes.js';
 import areasRoutes from '../routes/areas.routes.js'
 import cargosRoutes from '../routes/cargos.routes.js';
-import avataresRoutes from '../routes/avatares.routes.js';
 import fileUpload from 'express-fileupload';
 import swaggerUI from "swagger-ui-express";
 import swaggerDocument from '../swagger/swagger.json' assert {type: "json"}
@@ -45,7 +44,6 @@ class Server{
         this.app.use(this.paths.reportes, reportesRoutes);
         this.app.use(this.paths.areas, areasRoutes)
         this.app.use(this.paths.cargos, cargosRoutes);
-        this.app.use(this.paths.avatares, avataresRoutes);
         this.app.use("/api-doc/", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
     }
