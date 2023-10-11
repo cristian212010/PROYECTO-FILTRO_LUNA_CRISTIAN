df19c9f2f7174c66c8572373fd778fe05d2fc313
Yisuusss:3 <mrlgsus22@gmail.com>
2023-10-11 09:18:55 -0500

    feat: :sparkles: register

--- frontend/src/components/register/register.jsx
+++ frontend/src/components/register/register.jsx
@@ -39,6 +39,14 @@ const Register = () => {
     });
   };
 
+  const avatarChange = (event) => {
+    const file = event.target.files[0];
+    setFormData({
+      ...formData,
+      avatar: file,
+    });
+  };
+
   const submitRegister = async (event) => {
     event.preventDefault();
     const data = new FormData();
@@ -48,35 +56,16 @@ const Register = () => {
     data.append('password', formData.password);
     data.append('cargo', formData.cargo);
     data.append('avatar', formData.avatar);
-  
+
     try {
       console.log('Enviando solicitud de registro con los siguientes datos:', data);
       const response = await axios.post('http://localhost:6996/usuarios/insert', data);
       console.log('Respuesta del servidor después de registrar:', response.data);
       history.push('/login'); 
-      history.push('/login');
     } catch (error) {
       console.error('Error al registrar al usuario:', error);
     }
   };
-
-  const avatarChange = (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-  
-    reader.onload = (e) => {
-      const base64Image = e.target.result;
-      setFormData({
-        ...formData,
-        avatar: base64Image,
-      });
-    };
-  
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-  };
-  
 
   return (
     <div className='background'>
@@ -147,15 +136,6 @@ const Register = () => {
                   </option>
                 ))}
               </select>
-            </div>
-            <div className='div-input'>
-              <label>Avatar:</label>
-              <input
-                type="file"
-                accept="image/*"
-                onChange={avatarChange}
-                className='input-login'
-              />
             </div>
             <button type="submit" className='btn-login'>Registrar</button>
           </form>
