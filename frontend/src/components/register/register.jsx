3fc9333ecb0fa25433e9878af25b36cfb9cc296f
Yisuusss:3 <mrlgsus22@gmail.com>
2023-10-11 07:37:29 -0500

    feat: :sparkles: Try to use base64
    in register

--- frontend/src/components/register/register.jsx
+++ frontend/src/components/register/register.jsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useHistory, Link } from 'react-router-dom';
+import logo from '../../assets/img/KARIO_LOGO.png';
+import '../../assets/styles/register.css'
 
-const Registro = () => {
+const Register = () => {
+  const history = useHistory();
   const [formData, setFormData] = useState({
     nombre: '',
     apellido: '',
@@ -22,12 +26,12 @@ const Registro = () => {
         setCargos(cargos);
       })
       .catch((error) => {
-        console.error(error);
+        console.error('Error al cargar la lista de cargos:', error);
         setMensajeError('Error al cargar la lista de cargos');
       });
   }, []);
 
-  const change = (event) => {
+  const inputChange = (event) => {
     const { name, value } = event.target;
     setFormData({
       ...formData,
@@ -43,7 +47,7 @@ const Registro = () => {
     });
   };
 
-  const handleSubmit = async (event) => {
+  const submitRegister = async (event) => {
     event.preventDefault();
     const data = new FormData();
     data.append('nombre', formData.nombre);
@@ -52,90 +56,120 @@ const Registro = () => {
     data.append('password', formData.password);
     data.append('cargo', formData.cargo);
     data.append('avatar', formData.avatar);
-
+  
     try {
-      await axios.post('http://localhost:6996/usuarios/insert', data);
+      console.log('Enviando solicitud de registro con los siguientes datos:', data);
+      const response = await axios.post('http://localhost:6996/usuarios/insert', data);
+      console.log('Respuesta del servidor después de registrar:', response.data);
+      history.push('/login');
     } catch (error) {
-      console.error(error);
-      setMensajeError('Error al registrar al usuario');
+      console.error('Error al registrar al usuario:', error);
     }
   };
 
+  const handleAvatarChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+  
+    reader.onload = (e) => {
+      const base64Image = e.target.result;
+      setFormData({
+        ...formData,
+        avatar: base64Image,
+      });
+    };
+  
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+  
+
   return (
-    <div>
-      <h2>Registro de Usuario</h2>
-      {mensajeError && <p>{mensajeError}</p>}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Nombre:</label>
-          <input
-            type="text"
-            name="nombre"
-            value={formData.nombre}
-            onChange={change}
-            required
-          />
+    <div className='background'>
+      <div className='filter-login'>
+        <div className='form-register'>
+          <img src={logo} alt="KARIO" className='logo-login' />
+          <h2 className='h2-login'>Registro de Usuario</h2>
+          <p className='p-login'>Por favor ingresa los siguientes datos para ingresar a la plataforma</p>
+          {mensajeError && <p>{mensajeError}</p>}
+          <form onSubmit={submitRegister}>
+            <div className='div-input'>
+              <label>Nombre:</label>
+              <input
+                type="text"
+                name="nombre"
+                value={formData.nombre}
+                onChange={inputChange}
+                required
+                className='input-login'
+              />
+            </div>
+            <div className='div-input'>
+              <label>Apellido:</label>
+              <input
+                type="text"
+                name="apellido"
+                value={formData.apellido}
+                onChange={inputChange}
+                required
+                className='input-login'
+              />
+            </div>
+            <div className='div-input'>
+              <label>Usuario:</label>
+              <input
+                type="text"
+                name="usuario"
+                value={formData.usuario}
+                onChange={inputChange}
+                required
+                className='input-login'
+              />
+            </div>
+            <div className='div-input'>
+              <label>Password:</label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={inputChange}
+                required
+                className='input-login'
+              />
+            </div>
+            <div className='div-input'>
+              <label>Cargo:</label>
+              <select
+                name="cargo"
+                value={formData.cargo}
+                onChange={inputChange}
+                required
+                className='input-login'
+              >
+                <option value="">Seleccionar Cargo</option>
+                {cargos.map((cargo) => (
+                  <option key={cargo._id} value={cargo._id}>
+                    {cargo.cargo}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className='div-input'>
+              <label>Avatar:</label>
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleAvatarChange}
+                className='input-login'
+              />
+            </div>
+            <button type="submit" className='btn-login'>Registrar</button>
+          </form>
+          <p className='p-registrar'>¿Ya tienes una cuenta? <Link to="/login">Iniciar sesión</Link></p>
         </div>
-        <div>
-          <label>Apellido:</label>
-          <input
-            type="text"
-            name="apellido"
-            value={formData.apellido}
-            onChange={change}
-            required
-          />
-        </div>
-        <div>
-          <label>Usuario:</label>
-          <input
-            type="text"
-            name="usuario"
-            value={formData.usuario}
-            onChange={change}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={change}
-            required
-          />
-        </div>
-        <div>
-          <label>Cargo:</label>
-          <select
-            name="cargo"
-            value={formData.cargo}
-            onChange={change}
-            required
-          >
-            <option value="">Seleccionar Cargo</option>
-            {cargos.map((cargo) => (
-              <option key={cargo._id} value={cargo._id}>
-                {cargo.cargo}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label>Avatar:</label>
-          <input
-            type="file"
-            name="avatar"
-            onChange={avatarChange}
-            accept="image/*"
-            required
-          />
-        </div>
-        <button type="submit">Registrar</button>
-      </form>
+      </div>
     </div>
   );
 };
-
-export default Registro;
+export default Register
